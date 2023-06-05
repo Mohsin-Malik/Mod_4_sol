dba8ca9142bbec7cf0240415b844a38e88b7aba6
Mohsin-Malik <128085065+Mohsin-Malik@users.noreply.github.com>
2023-06-05 17:37:53 +0500

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,12 @@
+var array= ['jason'];
+for (var i=0; i<array.length; i++)
+{
+    if(array[0]== 'j' || array[0]== 'J')
+    {
+        console.log("Goodbye Jason");
+    }
+    else
+    {
+        console.log("Hello merry");
+    }
+}
